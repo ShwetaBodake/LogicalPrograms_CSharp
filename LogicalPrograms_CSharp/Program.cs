eea15e728cd64eaaf638e161d8ed6a8bde5382a7
Shweta Bodake <shwetabodake8108@gmail.com>
2023-01-26 14:32:53 +0530

    [Shweta] Added welcome message to Logical Program

---
+++ LogicalPrograms_CSharp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LogicalPrograms_CSharp
+{
+class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("WelCome to Logical Programs");
+        }
+    }
+}
+
