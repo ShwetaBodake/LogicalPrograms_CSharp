e0aaca610aae7522b4005c93d34e1bf75cd91003
Shweta Bodake <shwetabodake8108@gmail.com>
2023-01-27 11:44:45 +0530

    [Shweta] Modified Harmonic Series Number is Calculated

--- LogicalPrograms_CSharp/Program.cs
+++ LogicalPrograms_CSharp/Program.cs
@@ -1,11 +1,24 @@
 ﻿namespace LogicalPrograms_CSharp
 {
-class Program
+    class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("WelCome to Logical Programs");
+            Console.WriteLine("Enter the number ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int n1 = 0, n2 = 1, n3;
+            Console.WriteLine("Harmoni series :- {0}", n1);
+            Console.WriteLine("Harmoni series :- {0}", n2);
+            for (int i=0;i<=num;i++)
+            {
+                n3 = n1 + n2;
+                n1 = n2;
+                n2 = n3;
+
+             
+                Console.WriteLine("Harmoni series :- {0}",n3);
+            }
         }
     }
 }
-
