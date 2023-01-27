4ac1061a0ae393433263bedfae5e3b822194e375
Shweta Bodake <shwetabodake8108@gmail.com>
2023-01-27 18:45:45 +0530

    [Shweta] Modified Prime nmuber is checked

---
+++ LogicalPrograms_CSharp/PrimeNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace LogicalPrograms_CSharp
+{
+	public class PrimeNumber
+	{
+		public static void PrimeNumberDisplay()
+		{
+			int count = 0;
+			Console.WriteLine("Enter the nmuber");
+			int num = Convert.ToInt32(Console.ReadLine());
+			for (int i = 1; i <= num; i++)
+			{
+				if (num%i == 0)
+				{
+					count++;
+				}
+              
+            }
+            Console.WriteLine(count);
+            if (count==2)
+			{
+				Console.WriteLine("The given number {0} is prime number",num);
+			}
+		}
+	}
+}
+
--- LogicalPrograms_CSharp/Program.cs
+++ LogicalPrograms_CSharp/Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("WelCome to Logical Programs");
             Console.WriteLine("Choose any one program from below option: ");
-            Console.WriteLine("1:Harmonic Series \n 2:Perfect Nmuber");
+            Console.WriteLine("1:Harmonic Series \n 2:Perfect Nmuber \n 3:Prime Number");
             int Options=Convert.ToInt32(Console.ReadLine());
             switch (Options)
             {
@@ -15,6 +15,12 @@
                     break;
                 case 2:
                     PerfectNumber.PerfectNumberDisplay();
+                    break;
+                case 3:
+                    PrimeNumber.PrimeNumberDisplay();
+                    break;
+                default:
+                    Console.WriteLine("Enter option within given range");
                     break;
             }
         }
