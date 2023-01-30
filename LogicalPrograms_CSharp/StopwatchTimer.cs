a45fada36c3baf939d6d0ee9b38e36c363b2016b
Shweta Bodake <shwetabodake8108@gmail.com>
2023-01-30 23:51:50 +0530

    [Shweta] Modified Using  Stopwatch timer program elapsed time is calculated

---
+++ LogicalPrograms_CSharp/StopwatchTimer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace LogicalPrograms_CSharp
+{
+	public class StopwatchTimer
+	{
+        
+            static int stopTime = 0;
+            static int startTime = 0;
+            public static void start()
+            {
+                DateTime dt = DateTime.Now;
+                startTime = dt.Millisecond;
+
+            }
+
+            public static void stop()
+            {
+                DateTime dt = DateTime.Now;
+                stopTime = dt.Millisecond;
+
+            }
+
+            public static void stopWatch()
+            {
+                Console.WriteLine("Enter 1 to start the watch");
+                int startCounter = Convert.ToInt32(Console.ReadLine());
+                if (startCounter == 1)
+                {
+                    start();
+                }
+
+                Console.WriteLine("Enter 2 to start the watch");
+                int stopCounter = Convert.ToInt32(Console.ReadLine());
+                if (stopCounter == 2)
+                {
+                    stop();
+                }
+                int timeElapsed = startTime - stopTime;
+
+                Console.WriteLine("Time elapsed: " + timeElapsed);
+
+
+
+            }
+        }
+}
+
